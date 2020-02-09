02eedd6c9e8ee11ac110f331490b9b6c6a5366d3
Nathan McCrina <nathan.mccrina@outlook.com>
2020-02-09 00:59:48 -0600

    remove files from old static version of site

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
-app.use(express.static('wwwroot'))
-
-app.listen(port, () => console.log(`listening on http://localhost:${port}`))
